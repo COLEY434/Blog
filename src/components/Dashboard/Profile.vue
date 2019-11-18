048fa66f6c3dc92cf64f962986296d5dc3d188d6
Anieze Collins <chigoziecollins434@gmail.com>
2019-11-18 21:45:16 +0100

    Created the component for viewing user profile

---
+++ src/components/Dashboard/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+      <div class="col-md-5">
+          <div style="border: 1px solid black">
+              <i class="fas fa-user" style="font-size: 300px; margin-left: 70px;"></i>
+          </div>
+        </div>
+      <div class="col-md-7">
+          <div>
+              <b>Surname</b>: Anieze <br>
+              <b>Firstname</b>: Collins <br>
+              <b>Username</b>: Colley <br>
+              <b>Email</b>: chigoziecollins434@gmail.com <br>
+              <b>Age</b>: 25 <br>
+              <b>Gender</b>: Male<br>
+              <b>State</b>: Lagos <br>
+              <b>Country</b>: Nigeria <br>
+          </div>
+          
+      </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    
+</style>
