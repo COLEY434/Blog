b1cf81c04f84c8462f4d664856adb9ec4a87065b
Anieze Collins <chigoziecollins434@gmail.com>
2019-11-18 21:49:48 +0100

    Removed bootstrap row class from the router view

--- src/components/Dashboard/DashboardHeader.vue
+++ src/components/Dashboard/DashboardHeader.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="row" style="border: 1px solid blue; margin-right: 0px">
-      <div class="col-md-3">posts</div>
-      <div class="col-md-3">followers</div>
-      <div class="col-md-3">Likes</div>
-      <div class="col-md-3">Replies</div>
+<div>
+  <div class="row" style="margin-right: 0px">
+      <div class="col-md-3">
+        <div class="box-header" style="background-color: rgb(217, 28, 224);">
+          <h3 class="box-header-content">Post</h3> 
+          <h3 class="box-header-content">23</h3> 
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="box-header" style="background-color: rgb(40, 93, 207);">
+          <h3 class="box-header-content">Followers</h3>  
+          <h3 class="box-header-content">90</h3>  
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="box-header" style="background-color: rgb(56, 221, 139);">
+          <h3 class="box-header-content">Likes</h3>
+          <h3 class="box-header-content">54</h3>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="box-header" style="background-color: rgb(241, 76, 54);">
+          <h3 class="box-header-content">Replies</h3>
+          <h3 class="box-header-content">9</h3>
+        </div>
+      </div>
   </div>
+  <br>
+  <hr style="height: 2px; background-color: rgb(10, 42, 71);">
+
+  
+    <router-view></router-view>   
+  
+</div>
 </template>
 
 <script>
@@ -13,6 +41,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .box-header {
+    position: relative;
+    width: 100%;
+    height: 200px;
+    margin: 2px;
+    border-radius: 7px;
+    
+  }
 
+  .box-header-content {
+    color: white;
+    text-align: center;
+    padding-top: 60px;
+    
+  }
 </style>
